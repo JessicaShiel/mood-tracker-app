96dd4dd977ffa5d2ab2c56b450cb7e2f9add8276
Jessica Shiel <G00435923@atu.ie>
2025-04-25 15:18:58 +0100

    Setting Page

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,20 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule, IonContent, IonItem, IonLabel, IonToggle, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList } from '@ionic/angular/standalone';
+import { Device } from '@capacitor/device';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    IonicModule,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonToggle,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonList
+  ]
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+  deviceInfo: any = {};
+  isDarkMode = false;
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.deviceInfo = await Device.getInfo();
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    this.isDarkMode = prefersDark;
+    this.setTheme(this.isDarkMode);
   }
 
+  toggleDarkMode(event: any) {
+    this.setTheme(event.detail.checked);
+  }
+
+  private setTheme(enableDark: boolean) {
+    document.body.classList.toggle('dark', enableDark);
+  }
 }
+
