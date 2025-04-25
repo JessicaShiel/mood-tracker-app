163105980d1dc955a1524b339da0c9d40d33d5eb
Jessica Shiel <G00435923@atu.ie>
2025-04-25 08:23:18 +0100

    Updated Navigation Page

---
+++ src/app/pages/tabs/tabs.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.page.html',
+  styleUrls: ['./tabs.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterModule, IonicModule]
+})
+export class TabsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
