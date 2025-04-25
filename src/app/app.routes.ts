a9db66929850d819060e2a88ed6de15bae9865ad
Jessica Shiel <G00435923@atu.ie>
2025-04-25 07:37:06 +0100

    Navigation Bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,42 @@
 import { Routes } from '@angular/router';
+import { TabsPage } from './pages/tabs/tabs.page';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'tabs/home',
     pathMatch: 'full',
   },
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./home/home.page').then((m) => m.HomePage),
+      },
+      {
+        path: 'today',
+        loadComponent: () =>
+          import('./pages/today/today.page').then((m) => m.TodayPage),
+      },
+      {
+        path: 'history',
+        loadComponent: () =>
+          import('./pages/history/history.page').then((m) => m.HistoryPage),
+      },
+      {
+        path: 'settings',
+        loadComponent: () =>
+          import('./pages/settings/settings.page').then((m) => m.SettingsPage),
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ],
+  },
 ];
+
