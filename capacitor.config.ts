85964bb83cc3545f13ab18a3160d9e273c9ecbb9
Jessica Shiel <G00435923@atu.ie>
2025-04-25 06:35:27 +0100

    Initial commit: created Ionic mood tracker app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mood-tracker-app',
+  webDir: 'www'
+};
+
+export default config;
